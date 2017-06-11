d148d9a228caf9e7db3e386f563b748260d57492
Marie Dagan <mdagan03@gmail.com>
2017-06-11 17:19:55 -0500

    timestamp edit

--- db/schema.sql
+++ db/schema.sql
@@ -6,6 +6,6 @@ CREATE TABLE products(
 	item_id int NOT NULL AUTO_INCREMENT,
 	burger_name varchar(30) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
-	date TIMESTAMP,
+	date timestamp DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (item_id)
 );
