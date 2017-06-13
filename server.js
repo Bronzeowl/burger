1365da0927692ce2032ac7f548a83781c5c38e66
Marie Dagan <mdagan03@gmail.com>
2017-06-13 18:32:54 -0500

    server.js modifications

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
-var port = 3000 || env.process.port;
-
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
@@ -23,7 +21,7 @@ app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
 var routes = require("./controllers/burgers_controller");
-
 app.use("/", routes);
 
+var port = 3000 || env.process.port;
 app.listen(port);
